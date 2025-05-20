4a63e3d4fd0a55726bda500924e925824d9fc88d
May Sunktong <pattaranan.sunktong@gmail.com>
2025-05-20 23:52:20 +0200

    Delete preinstalled font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Re-work",
-  description: "Reshape the way we work",
+  description: "Make Remote Work Possible",
 };
 
 export default function RootLayout({
@@ -25,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Typography } from "@mui/material";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Typography>Typography</Typography>
+    </div>
   );
 }

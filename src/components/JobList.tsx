d481fc75f5ea074d19f9bae7886772f310a8aa25
May Sunktong <pattaranan.sunktong@gmail.com>
2025-05-24 03:03:19 +0200

    🎨 Delete Grid and use Box instead

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,27 +1,34 @@
-import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import JobCard from "./UI/JobCard";
 
 const JobList = ({ jobs }: { jobs: Job[] }) => {
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{ width: "80%", mx: "auto", py: 2 }}
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: {
+          xs: "1fr",
+          sm: "1fr 1fr",
+          md: "1fr 1fr 1fr",
+        },
+        gap: 2,
+        width: "80%",
+        mx: "auto",
+        py: 2,
+      }}
     >
       {jobs.map((job) => (
-        <Grid
-          item
+        <Box
           key={job.id}
-          xs={12}
-          sm={6}
-          md={4}
-          sx={{ display: "flex" }}
-          flexGrow={1}
+          sx={{
+            display: "flex",
+          }}
         >
           <JobCard job={job} />
-        </Grid>
+        </Box>
       ))}
-    </Grid>
+    </Box>
+
   );
 }
 export default JobList;

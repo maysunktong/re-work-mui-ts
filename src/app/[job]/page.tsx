a1eaabbec41bab36d0dd58f024bfea75f5184214
May Sunktong <pattaranan.sunktong@gmail.com>
2025-05-25 17:19:48 +0200

    Take out Async Await

--- src/app/[job]/page.tsx
+++ src/app/[job]/page.tsx
@@ -1,64 +1,60 @@
 import { Typography, Box, Button, Chip, Stack, Avatar } from "@mui/material";
 import fetchJobs from "../../services/api";
 
-const JobPage = async ({ params }: { params: { job: string } }) => {
-  const jobs: Job[] = await fetchJobs();
-  const job = jobs.find((item) => item.id.toString() === params.job);
+const JobPage = ({ params }: { params: { job: string } }) => {
+  return fetchJobs().then((jobs) => {
+    const job = jobs.find((item) => item.id.toString() === params.job);
 
-  if (!job) {
-    return <Typography>Job not found</Typography>;
-  }
+    if (!job) {
+      return <Typography>Job not found</Typography>;
+    }
 
-  return (
-    <Box
-      sx={{
-        gap: 2,
-        width: "80%",
-        mx: "auto",
-        py: "6rem",
-        color: "gray",
-      }}
-    >
-      <Button type="button" href="/" variant="contained">
-        Back
-      </Button>
-      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Box>
-          <Typography fontWeight={500} color="primary.dark" variant="h4" pt={3}>
-            {job.title}
-          </Typography>
-          <Typography
-            fontWeight={400}
-            color="primary.light"
-            variant="h6"
-            py={2}
-          >
-            {job.company_name}
-          </Typography>
+    return (
+      <Box
+        sx={{
+          gap: 2,
+          width: "80%",
+          mx: "auto",
+          py: "6rem",
+          color: "gray",
+        }}
+      >
+        <Button type="button" href="/" variant="contained">
+          Back
+        </Button>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box>
+            <Typography fontWeight={500} color="primary.dark" variant="h4" pt={3}>
+              {job.title}
+            </Typography>
+            <Typography fontWeight={400} color="primary.light" variant="h6" py={2}>
+              {job.company_name}
+            </Typography>
+          </Box>
+          <Avatar
+            variant="square"
+            alt="company-logo"
+            src={job.company_logo}
+            sx={{ height: 100, width: 100 }}
+          />
         </Box>
-        <Avatar
-          variant="square"
-          alt="company-logo"
-          src={job.company_logo}
-          sx={{ height: 100, width: 100 }}
+
+        <Stack direction="row" spacing={1} flexWrap="wrap" mb={1}>
+          <Chip label={job.category} variant="outlined" />
+          <Chip
+            color="primary"
+            label={job.job_type === "full_time" ? "Full time" : job.job_type}
+            variant="outlined"
+          />
+        </Stack>
+        <Typography
+          dangerouslySetInnerHTML={{
+            __html: job.description,
+          }}
         />
       </Box>
-
-      <Stack direction="row" spacing={1} flexWrap="wrap" mb={1}>
-        <Chip label={job.category} variant="outlined" />
-        <Chip
-          color="primary"
-          label={job.job_type === "full_time" ? "Full time" : job.job_type}
-          variant="outlined"
-        />
-      </Stack>
-      <Typography
-        dangerouslySetInnerHTML={{
-          __html: job.description,
-        }}
-      />
-    </Box>
-  );
-};
+    );
+  });
+}
 
 export default JobPage;

62ddf44104f39056abddd7c60947b451e03f9895
May Sunktong <pattaranan.sunktong@gmail.com>
2025-05-25 16:40:14 +0200

    Attempt to fix PageProps for JobPage

--- src/app/[job]/page.tsx
+++ src/app/[job]/page.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Button, Chip, Stack} from "@mui/material";
+import { Typography, Box, Button, Chip, Stack, CardMedia } from "@mui/material";
 import fetchJobs from "../../services/api";
 
 export default async function JobPage({ params }: JobPageProps) {
@@ -22,12 +22,28 @@ export default async function JobPage({ params }: JobPageProps) {
       <Button type="button" href="/" variant="contained">
         Back
       </Button>
-      <Typography fontWeight={500} color="primary.dark" variant="h4" pt={3}>
-        {job.title}
-      </Typography>
-      <Typography fontWeight={400} color="primary.light" variant="h6" py={2}>
-        {job.company_name}
-      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "space-between", alignItem: "center" }}>
+        <Box>
+          <Typography fontWeight={500} color="primary.dark" variant="h4" pt={3}>
+            {job.title}
+          </Typography>
+          <Typography
+            fontWeight={400}
+            color="primary.light"
+            variant="h6"
+            py={2}
+          >
+            {job.company_name}
+          </Typography>
+        </Box>
+        <CardMedia
+          component="img"
+          height="194"
+          image={job.company_logo}
+          alt={job.company_name}
+        />
+      </Box>
+
       <Stack direction="row" spacing={1} flexWrap="wrap" mb={1}>
         <Chip label={job.category} variant="outlined" />
         <Chip

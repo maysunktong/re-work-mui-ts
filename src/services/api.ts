f228b6e7ef1ce5edb178262954442d8e20c1c3c6
May Sunktong <pattaranan.sunktong@gmail.com>
2025-05-29 13:09:55 +0200

    Fix api call to get category

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-const fetchJobs = async (): Promise<Job[]> => {
-  const API_URL = `https://remotive.com/api/remote-jobs`;
+const fetchJobs = async (category: string = ""): Promise<Job[]> => {
+  const API_URL = `https://remotive.com/api/remote-jobs${category ? `?category=${encodeURIComponent(category)}` : ""}`;
   const res: Response = await fetch(API_URL);
   if (!res.ok) throw new Error(`Failed to fetch. Status: ${res.status}`);
   const jobData = await res.json();

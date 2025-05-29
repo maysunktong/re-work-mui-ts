da5f434bb9e45eb0b7cb7e889a1c43018283ee9a
May Sunktong <pattaranan.sunktong@gmail.com>
2025-05-29 11:34:44 +0200

    Better api by taking out try catch

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,16 +1,9 @@
-const fetchJobs = async (category: string = ""): Promise<Job[]> => {
-  const API_URL = `https://remotive.com/api/remote-jobs${category ? `?category=${encodeURIComponent(category)}` : ""}`;
-  try {
-    const res: Response = await fetch(API_URL);
-    if (!res.ok) {
-      throw new Error(`Failed to fetch jobs. Status: ${res.status}`);
-    }
-    const jobData = await res.json();
-    return jobData.jobs || [];
-  } catch (error) {
-    console.error("Error fetching jobs:", error);
-    return [];
-  }
+const fetchJobs = async (): Promise<Job[]> => {
+  const API_URL = `https://remotive.com/api/remote-jobs`;
+  const res: Response = await fetch(API_URL);
+  if (!res.ok) throw new Error(`Failed to fetch. Status: ${res.status}`);
+  const jobData = await res.json();
+  return jobData.jobs;
 };
 
 export default fetchJobs;
